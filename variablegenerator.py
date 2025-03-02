e074ec8768e937880be704a5de334b48f9bee509
SypTitan <sybrentitan@gmail.com>
2025-03-02 23:53:02 +0100

    Added global namespace

--- variablegenerator.py
+++ variablegenerator.py
@@ -1,26 +1,29 @@
 from variableprocessor import ID, INDIVIDUAL, GROUPS
 
-def generate_individuals(data: dict[str, dict[str, str|list[str]]], line_start: str = "  ") -> str:
+def generate_individuals(data: dict[str, dict[str, str|list[str]]], line_start: str = "  ", namespace: str = "global") -> str:
     out: str = ""
     
     for i in data:
         if (type(data[i][INDIVIDUAL]) == str):
-            out += line_start + f"{data[i][INDIVIDUAL]}: {data[i][ID]}" + '\n'
+            # Single individual ID
+            out += line_start + f"{namespace}|{data[i][INDIVIDUAL]}: {data[i][ID]}" + '\n'
+            
         elif (type(data[i][INDIVIDUAL]) == list):
+            # Multiple individual IDs
             for j in data[i][INDIVIDUAL]:
-                out += line_start + f"{j}: {data[i][ID]}" + '\n'
+                out += line_start + f"{namespace}|{j}: {data[i][ID]}" + '\n'
         else:
             raise ValueError(f"Unknown type: {type(data[i][INDIVIDUAL])}")
         
     return out
 
-def generate_groups(data: dict[str, dict[str, str|list[str]]], line_start: str = "  ") -> str:    
+def generate_groups(data: dict[str, dict[str, str|list[str]]], line_start: str = "  ", namespace: str = "global") -> str:    
     out: str = ""
     
     groups: dict[str, list[str]] = group_data(data)
     
     for i in groups:
-        out += line_start + f"{i}: {', '.join(groups[i])}" + '\n'
+        out += line_start + f"{namespace}|{i}: {', '.join(groups[i])}" + '\n'
         
     return out
 
